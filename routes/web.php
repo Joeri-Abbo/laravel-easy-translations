686e9d3e108fcf75f1ae9607020d43a5af761a4d
Joeri Abbo <joeriabbo@hotmail.com>
2022-05-29 23:04:27 +0200

    Change route.

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use JoeriAbbo\LaravelEasyTranslations\LaravelEasyTranslationsPackageServiceProvider;
 
-Route::group(['prefix' => 'businesses', 'namespace' => 'Languages', 'as' => LaravelEasyTranslationsPackageServiceProvider::PACKAGE_NAME . '.'], function () {
+Route::group(['prefix' => LaravelEasyTranslationsPackageServiceProvider::PACKAGE_NAME, 'namespace' => 'Languages', 'as' => LaravelEasyTranslationsPackageServiceProvider::PACKAGE_NAME . '.'], function () {
     Route::get('/', 'Index')->name('index');
     Route::get('/edit/{language}', 'Edit')->name('edit');
     Route::put('/edit/{language}', 'Update')->name('update');
