644aa0ee7b8a7b7cff13a6a5a95bc54ef7ed8618
Joeri Abbo <joeriabbo@hotmail.com>
2022-06-03 00:03:26 +0200

    End of day commit.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
-    content: ["./resources/views/*.{html,php,js}","./resources/views/pages/*.{html,php,js}"],
+    content: [
+        "./resources/views/*.{html,php,js}",
+        "./resources/views/pages/*.{html,php,js}",
+        "./node_modules/flowbite/**/*.js"
+    ],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [
+        require('flowbite/plugin')
+    ],
 }
