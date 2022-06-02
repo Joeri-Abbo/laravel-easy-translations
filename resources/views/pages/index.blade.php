3ed8ca9a3adeac2b3aa94c3daa404195a324b439
Joeri Abbo <joeriabbo@hotmail.com>
2022-06-02 23:39:40 +0200

    Add assets with mix manifest.

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- Meta Information -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="robots" content="noindex, nofollow">
     <title>Laravel Easy Translations {{ config('app.name') ? ' - ' . config('app.name') : '' }}</title>
-
+    <link rel="stylesheet" href="{{asset(mix('/css/app.css', 'vendor/laravel-easy-translations'))}}">
+    <script src="{{asset(mix('/js/app.js', 'vendor/laravel-easy-translations'))}}"></script>
 </head>
 <body>
 <div id="telescope">
-    <div class="bg-white">
+    <div class="bg-red-600">
+        I Love You In Every Universe
         @yield('content')
     </div>
 </div>
