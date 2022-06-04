d6234353f68a7b4ddef01d1ac0ea8763655685f3
Joeri Abbo <joeriabbo@hotmail.com>
2022-06-04 23:27:15 +0200

    Test with new styling.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
     content: [
         "./resources/views/*.{html,php,js}",
         "./resources/views/pages/*.{html,php,js}",
+        "./resources/views/components/*.{html,php,js}",
         "./node_modules/flowbite/**/*.js"
     ],
     theme: {
